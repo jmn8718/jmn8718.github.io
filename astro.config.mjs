2c81c867a6ff0830297a130b5bec7f0f0da3b550
Jose Miguel Navarro <jmn8718@gmail.com>
2026-03-06 21:09:23 +0900

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jmn8718.github.io/'
+  site: 'https://jmn8718.github.io/',
+  integrations: [
+    icon({
+      include: {
+        'fa6-solid': ['desktop', 'envelope'],
+        'fa6-brands': ['github', 'gitlab', 'linkedin', 'medium'],
+      },
+    }),
+  ],
 });
